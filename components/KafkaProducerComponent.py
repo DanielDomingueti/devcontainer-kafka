7dfac3d100c8430f0b6e19bf01b84429cf0b9a50
DanielDomingueti <danieldomingueti@hotmail.com>
2023-08-01 00:29:13 +0000

    docker compose config

--- components/KafkaProducerComponent.py
+++ components/KafkaProducerComponent.py
@@ -1,21 +1,21 @@
 import json
 import time
-from kafka import KafkaProducer
+from confluent_kafka import Producer
 
-bootstrap_server = 'localhost:9092'
-topic = 'onboarding_email'
-kafkaProducer = KafkaProducer(bootstrap_servers=bootstrap_server)
-
-message_limit = 5
+bootstrap_servers = 'localhost:29092'
 topic = 'onboarding_email'
 
-for i in range(message_limit):
+producer = Producer({'bootstrap.servers': bootstrap_servers})
+
+for i in range(5):  # Você pode ajustar o limite aqui, se desejar
     message = {
         "message_id": i,
-        "message_user_email": "client{i}@email.com",
-        "message_body": "Onboarding message"
+        "message_subject": f"Onboarding subject {i}",
+        "message_body": f"Onboarding message {i}"
     }
-    kafkaProducer.send(topic, json.dumps(message).encode("UTF-8"))
+    producer.produce(topic, json.dumps(message).encode("UTF-8"))
+    producer.flush()
     print(f"Sent email: {i}")
     time.sleep(1)
 
+producer.flush()
